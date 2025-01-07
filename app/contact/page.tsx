1f43b2ad4a005558145bad32caaf661aff3f27f8
norbertvajko <norbertvajko@yahoo.com>
2025-01-07 21:50:42 +0200

    fixes

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,25 +3,26 @@ import contactModelImg from "../../assets/images/model-contact.png";
 import { Suspense } from "react";
 import PageLoader from "@/components/page-loader";
 import { ContactForm } from "./_components/ContactForm";
-import { useIsMobile } from "@/hooks/use-is-mobile";
 
 const ContactPage = async () => {
-
-	const isMobile = useIsMobile();
-
 	return (
 		<Suspense fallback={<PageLoader />}>
 			<div className="flex flex-col">
 				<div className="flex flex-col md:flex-row bg-black h-screen">
-					<div className={isMobile ? "md:w-2/5 w-full h-1/2 md:h-full mb-[208px]" : "md:w-2/5 w-full h-1/2 md:h-full"}>
-						<img src={contactModelImg.src} alt="Contact Model" className="w-full h-full object-contain pt-[74px] md:pt-0" />
+					{/* Apply margin-bottom only on mobile screens (below md) */}
+					<div className="md:w-2/5 w-full h-1/2 md:h-full mb-[208px] md:mb-0">
+						<img
+							src={contactModelImg.src}
+							alt="Contact Model"
+							className="w-full h-full object-contain pt-[74px] md:pt-0"
+						/>
 					</div>
 					<ContactForm />
 				</div>
 				<NewsletterSection />
 			</div>
 		</Suspense>
-	)
+	);
 };
 
 export default ContactPage;
