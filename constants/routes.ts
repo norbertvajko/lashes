1f9c952f243cbec90770f38a459bc4c64c4f3c58
norbertvajko <norbertvajko@yahoo.com>
2025-01-12 04:25:44 +0200

    fixes

--- constants/routes.ts
+++ constants/routes.ts
@@ -8,24 +8,3 @@ export const APP_ROUTES = {
     PRODUCT: "/product",
     PRODUCT_DETAILS: "/product/[slug]"
 };
-
-export const APP_ROUTES_AUTH = {
-    LOGIN: "/auth/login",
-    REGISTER: "/auth/register",
-    RESET: "/auth/reset",
-    ERROR: "/auth/error",
-    NEW_PASSWORD: "/auth/new-password",
-}
-
-export const APP_ROUTES_USER = {
-    USER: "/user",
-    ACCOUNT: "/user/my-account",
-    SETTINGS: "/user/my-account/settings",
-    SETTINGS_PROFILE: "/user/my-account/settings/profile",
-    SETTINGS_MANAGE_ACCOUNT: "/user/my-account/settings/manage-account",
-}
-
-export const APP_ROUTES_ADMIN = {
-    ADMIN: "/admin",
-    SCRAPE: "/admin/scrape",
-}
